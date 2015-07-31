448c2cb970ceb7d8019175a0d8f3159d12af3068
Tenghuan He <tenghuanhe@gmail.com>
2015-07-31 11:50:11 +0800

    /

--- pc.c
+++ pc.c
@@ -95,57 +95,58 @@ queue* queueInit(void)
 {
     queue* q;
 
-    q = (queue*)malloc (sizeof (queue));
-    if (q == NULL) return (NULL);
+    q = (queue*)malloc(sizeof(queue));
+    if(q == NULL)
+        return(NULL);
 
     q->empty = 1;
     q->full = 0;
     q->head = 0;
     q->tail = 0;
-    q->mut = (pthread_mutex_t*) malloc (sizeof (pthread_mutex_t));
+    q->mut = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
     pthread_mutex_init (q->mut, NULL);
-    q->notFull = (pthread_cond_t*) malloc (sizeof (pthread_cond_t));
+    q->notFull = (pthread_cond_t*) malloc(sizeof(pthread_cond_t));
     pthread_cond_init (q->notFull, NULL);
-    q->notEmpty = (pthread_cond_t*) malloc (sizeof (pthread_cond_t));
-    pthread_cond_init (q->notEmpty, NULL);
+    q->notEmpty = (pthread_cond_t*) malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(q->notEmpty, NULL);
 
     return (q);
 }
 
 void queueDelete (queue* q)
 {
-	pthread_mutex_destroy (q->mut);
-	free (q->mut);	
-	pthread_cond_destroy (q->notFull);
-	free (q->notFull);
-	pthread_cond_destroy (q->notEmpty);
-	free (q->notEmpty);
-	free (q);
+    pthread_mutex_destroy(q->mut);
+    free(q->mut);	
+    pthread_cond_destroy(q->notFull);
+    free(q->notFull);
+    pthread_cond_destroy(q->notEmpty);
+    free(q->notEmpty);
+    free(q);
 }
 
 void queueAdd (queue* q, int in)
 {
-	q->buf[q->tail] = in;
-	q->tail++;
-	if (q->tail == QUEUESIZE)
-		q->tail = 0;
-	if (q->tail == q->head)
-		q->full = 1;
-	q->empty = 0;
+    q->buf[q->tail] = in;
+    q->tail++;
+    if(q->tail == QUEUESIZE)
+        q->tail = 0;
+    if(q->tail == q->head)
+        q->full = 1;
+    q->empty = 0;
 
-	return;
+    return;
 }
 
-void queueDel (queue* q, int* out)
+void queueDel(queue* q, int* out)
 {
-	*out = q->buf[q->head];
+    *out = q->buf[q->head];
+    
+    q->head++;
+    if(q->head == QUEUESIZE)
+    q->head = 0;
+    if(q->head == q->tail)
+        q->empty = 1;
+    q->full = 0;
 
-	q->head++;
-	if (q->head == QUEUESIZE)
-		q->head = 0;
-	if (q->head == q->tail)
-		q->empty = 1;
-	q->full = 0;
-
-	return;
+    return;
 }
