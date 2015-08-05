9f836eeaf379f6c922040eb01189bade14e12766
Tenghuan He <tenghuanhe@gmail.com>
2015-08-05 11:48:17 -0700

    /////

--- 2.c
+++ 2.c
@@ -9,53 +9,52 @@ static size_t read_callback(void* ptr, size_t size, size_t nmemb, void* stream);
 
 int main()
 {
-	char* server = "https://tenghuanhe:hetenghuan@tenghuanhe.cloudant.com/cctv11/1/1.jpg?rev=1-c181e911a2584dccf0c056f6265966b9";
-	char* file = "1.jpg";
+    char* server = "https://tenghuanhe:hetenghuan@tenghuanhe.cloudant.com/cctv11/1/1.jpg?rev=1-c181e911a2584dccf0c056f6265966b9";
+    char* file = "1.jpg";
 
-	http_put_file(file, server);
-
-	return 0;
+    http_put_file(file, server);
+    return 0;
 }
 
 void http_put_file(char* file, char* server_target)
 {
-	FILE* fp;
-	struct curl_slist* header_list = NULL;
-	CURL* curl_handle;
-	CURLcode res;
-	struct stat file_info;
+    FILE* fp;
+    struct curl_slist* header_list = NULL;
+    CURL* curl_handle;
+    CURLcode res;
+    struct stat file_info;
 
-	stat(file, &file_info);
-	fp = fopen(file, "rb");
-	curl_handle = curl_easy_init();
-	header_list = curl_slist_append(header_list, "Content-Type: image/jpeg");
+    stat(file, &file_info);
+    fp = fopen(file, "rb");
+    curl_handle = curl_easy_init();
+    header_list = curl_slist_append(header_list, "Content-Type: image/jpeg");
 
-	if (curl_handle)
-	{
-		curl_easy_setopt(curl_handle, CURLOPT_READFUNCTION, read_callback);
-		curl_easy_setopt(curl_handle, CURLOPT_UPLOAD, 1L);
-		curl_easy_setopt(curl_handle, CURLOPT_PUT, 1L);
-		curl_easy_setopt(curl_handle, CURLOPT_URL, server_target);
-		curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, header_list);
-		curl_easy_setopt(curl_handle, CURLOPT_READDATA, fp);
-		curl_easy_setopt(curl_handle, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_info.st_size);
+    if (curl_handle)
+    {
+        curl_easy_setopt(curl_handle, CURLOPT_READFUNCTION, read_callback);
+        curl_easy_setopt(curl_handle, CURLOPT_UPLOAD, 1L);
+        curl_easy_setopt(curl_handle, CURLOPT_PUT, 1L);
+        curl_easy_setopt(curl_handle, CURLOPT_URL, server_target);
+        curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, header_list);
+        curl_easy_setopt(curl_handle, CURLOPT_READDATA, fp);
+        curl_easy_setopt(curl_handle, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_info.st_size);
 
-		res = curl_easy_perform(curl_handle);
-	}
+        res = curl_easy_perform(curl_handle);
+    }
 
-	fclose(fp);
-	curl_easy_cleanup(curl_handle);
+    fclose(fp);
+    curl_easy_cleanup(curl_handle);
 }
 
 static size_t read_callback(void* ptr, size_t size, size_t nmemb, void* stream)
 {
-	size_t retcode;
-	curl_off_t nread;
+    size_t retcode;
+    curl_off_t nread;
 
-	retcode = fread(ptr, size, nmemb, stream);
+    retcode = fread(ptr, size, nmemb, stream);
 
-	nread = (curl_off_t)retcode;
-	fprintf(stderr, "***We read %" CURL_FORMAT_CURL_OFF_T" bytes from file\n", nread);
+    nread = (curl_off_t)retcode;
+    fprintf(stderr, "***We read %" CURL_FORMAT_CURL_OFF_T" bytes from file\n", nread);
 
-	return retcode;
+    return retcode;
 }
--- sf.c
+++ sf.c
@@ -15,13 +15,15 @@ struct memory_chunk
 
 pt_node_t* get_gps_doc();
 pt_node_t* get_log_doc();
-pt_node_t* get_key_frame_doc();
+pt_node_t* get_keyframe_doc();
 static void* myrealloc(void* ptr, size_t size);
 static size_t recv_memory_callback(void* ptr, size_t size, size_t nmemb, void* data);
-void http_post_data(char* data, int data_len, char* server_target);
+char* http_post_data(char* data, int data_len, char* server_target);
+static size_t read_callback(void* ptr, size_t size, size_t nmemb, void* stream);
+void http_put_file(char* file, char* server_target);
 void upload_local_as_bulks(char* file, char* server_target);
-void upload_a_single_doc(pt_node_t* doc, char* server_target);
-void upload_keyframe_doc_with_attachment(pt_node_t* doc, char* filename);
+char* upload_a_single_doc(pt_node_t* doc, char* server_target);
+void upload_doc_with_keyframe_as_attachment(pt_node_t* doc, char* filename, char* server_target);
 void save_gps_to_local(pt_node_t* gps, char* file);
 
 double getLon();
@@ -34,56 +36,59 @@ char* getTime();
 
 int main(int argc, char** argv)
 {
-    char* server_target = "http://tenghuanhe:hetenghuan@tenghuanhe.cloudant.com/cctv11";
+    char* server_target = "http://tenghuanhe:hetenghuan@tenghuanhe.cloudant.com/cctv12";
     pt_node_t* gps_doc = get_gps_doc();
-	pt_node_t* log_doc = get_log_doc();
+    pt_node_t* log_doc = get_log_doc();
+    pt_node_t* keyframe_doc = get_keyframe_doc();
 
     upload_a_single_doc(gps_doc, server_target);
     upload_a_single_doc(log_doc, server_target);
+    upload_doc_with_keyframe_as_attachment(keyframe_doc, "keyframe.jpg", server_target);
+
     return 0; 
 }
 
 pt_node_t* get_gps_doc()
 {
-	char* time = getTime();
-	pt_node_t* gps_doc = pt_map_new();
+    char* time = getTime();
+    pt_node_t* gps_doc = pt_map_new();
 
-	pt_map_set(gps_doc, "_id", pt_string_new(time));
-	pt_map_set(gps_doc, "ENGID", pt_integer_new(getEngID()));
-	pt_map_set(gps_doc, "GpsType", pt_integer_new(getGpsType()));
-	pt_map_set(gps_doc, "Longtitude", pt_double_new(getLon()));
-	pt_map_set(gps_doc, "Latitude", pt_double_new(getLat()));
-	pt_map_set(gps_doc, "GpsTime", pt_string_new(time));
+    pt_map_set(gps_doc, "ENGID", pt_integer_new(getEngID()));
+    pt_map_set(gps_doc, "GpsType", pt_integer_new(getGpsType()));
+    pt_map_set(gps_doc, "Longtitude", pt_double_new(getLon()));
+    pt_map_set(gps_doc, "Latitude", pt_double_new(getLat()));
+    pt_map_set(gps_doc, "GpsTime", pt_string_new(time));
 
-	return gps_doc;
+    return gps_doc;
 }
 
 pt_node_t* get_log_doc()
 {
-	char* time = getTime();
-	pt_node_t* log_doc = pt_map_new();
-	pt_node_t* gps = pt_map_new();
+    char* time = getTime();
+    pt_node_t* log_doc = pt_map_new();
+    pt_node_t* gps = pt_map_new();
 
-	pt_map_set(gps, "Longtitude", pt_integer_new(getLon()));
-	pt_map_set(gps, "Latitude", pt_integer_new(getLat()));
-	pt_map_set(gps, "GpsTime", pt_string_new(time));
+    pt_map_set(gps, "Longtitude", pt_integer_new(getLon()));
+    pt_map_set(gps, "Latitude", pt_integer_new(getLat()));
+    pt_map_set(gps, "GpsTime", pt_string_new(time));
 
-	pt_map_set(log_doc, "ENGID", pt_integer_new(getEngID()));
-	pt_map_set(log_doc, "GPS", gps);
-	pt_map_set(log_doc, "OperationTypeID", pt_integer_new(getOperationTypeID()));
-	pt_map_set(log_doc, "Description", pt_string_new(getDescription()));
-	pt_map_set(log_doc, "OperationTime", pt_string_new(time));
+    pt_map_set(log_doc, "ENGID", pt_integer_new(getEngID()));
+    pt_map_set(log_doc, "GPS", gps);
+    pt_map_set(log_doc, "OperationTypeID", pt_integer_new(getOperationTypeID()));
+    pt_map_set(log_doc, "Description", pt_string_new(getDescription()));
+    pt_map_set(log_doc, "OperationTime", pt_string_new(time));
 
-	return log_doc;
+    return log_doc;
 }
 
 pt_node_t* get_keyframe_doc()
 {
-	pt_node_t* key_frame_doc = pt_map_new();
-	pt_node_t* attachment = pt_map_new();
+    char* time = getTime();
+    pt_node_t* keyframe_doc = pt_map_new();
 
-	pt_map_set(key_frame_doc, "ENGID", pt_integer_new(getEngID()));
-	pt_map_set(key_frame_doc, "_attachments", attachment);
+    pt_map_set(keyframe_doc, "ENGID", pt_integer_new(getEngID()));
+    pt_map_set(keyframe_doc, "UploadTime", pt_string_new(time));
+    return keyframe_doc;
 }
 
 double getLon()
@@ -98,25 +103,25 @@ double getLat()
 
 int getEngID()
 {
-	return 1;
+    return 1;
 }
 
 int getGpsType()
 {
-	return 1;
+    return 1;
 }
 
 int getOperationTypeID()
 {
-	return 2;
+    return 2;
 }
 
 char* getDescription()
 {
-	char* des= malloc(sizeof(char) * 256);
-	des= "This is the description part";
+    char* des= malloc(sizeof(char) * 256);
+    des= "This is the description part";
 
-	return des;
+    return des;
 }
 
 char* getTime()
@@ -128,10 +133,11 @@ char* getTime()
     return (char*)string;
 }
 
-void http_post_data(char* data, int data_len, char* server_target)
+char* http_post_data(char* data, int data_len, char* server_target)
 {
     struct curl_slist* header_list = NULL;
     struct memory_chunk chunk;
+    char* response;
     CURL *curl_handle = curl_easy_init();
     CURLcode res;
     chunk.memory = NULL;
@@ -147,7 +153,12 @@ void http_post_data(char* data, int data_len, char* server_target)
 
     res = curl_easy_perform(curl_handle);
     printf("%s\n", chunk.memory);
+    response = malloc(sizeof(char) * (chunk.size + 1));
+    memcpy(response, chunk.memory, chunk.size);
+    response[chunk.size] = '\0';
     curl_easy_cleanup(curl_handle);
+
+    return response;
 }
 
 static void* myrealloc(void* ptr, size_t size)
@@ -212,16 +223,17 @@ void upload_local_as_bulks(char* file, char* server_target)
     http_post_data(data, data_len, server_bulk_target);
 }
 
-void upload_a_single_doc(pt_node_t* doc, char* server_target)
+char* upload_a_single_doc(pt_node_t* doc, char* server_target)
 {
     char* data = NULL;
+    char* response;
     int data_len = 0;
     data = pt_to_json(doc, 2);
     data_len = strlen(data);
 
-	printf("%s\n", data);
+    printf("%s\n", data);
 
-    http_post_data(data, data_len, server_target);
+    response = http_post_data(data, data_len, server_target);
 }
 
 void save_gps_to_local(pt_node_t* gps, char* file)
@@ -232,7 +244,76 @@ void save_gps_to_local(pt_node_t* gps, char* file)
     data_len = strlen(data);
 }
 
-void upload_keyframe_doc_as_attachment(pt_node_t* doc, char* filename)
+void upload_doc_with_keyframe_as_attachment(pt_node_t* keyframe_doc, char* filename, char* server_target)
 {
-	
+    pt_node_t* resp_json;
+    char* update_url;
+    char* response;
+    const char* id;
+    const char* rev;
+    int url_len = 0;
+    response = upload_a_single_doc(keyframe_doc, server_target);
+    resp_json = pt_from_json(response);
+    id = pt_string_get(pt_map_get(resp_json, "id"));
+    rev = pt_string_get(pt_map_get(resp_json, "rev"));
+
+    printf("%s\n", id);
+    printf("%s\n", rev);
+    url_len = strlen(server_target) + strlen(id) + strlen(rev) + 7;
+    update_url = malloc(sizeof(char) * (strlen(server_target) + strlen(id) + strlen(rev) + 7));
+    memcpy(update_url, server_target, strlen(server_target));
+    memcpy(update_url + strlen(server_target), "/", 1);
+    memcpy(update_url + strlen(server_target) + 1, id, strlen(id));
+    memcpy(update_url + strlen(server_target) + 1 + strlen(id), "/", 1);
+    memcpy(update_url + strlen(server_target) + 1 + strlen(id) + 1, filename, strlen(filename));
+    memcpy(update_url + strlen(server_target) + 1 + strlen(id) + 1 + strlen(filename), "?rev=", 5);
+    memcpy(update_url + strlen(server_target) + 1 + strlen(id) + 1 + strlen(filename) + 5, rev, strlen(rev));
+
+    printf("%s\n", update_url);
+
+    http_put_file(filename, update_url);
 }
+
+void http_put_file(char* file, char* server_target)
+{
+    FILE* fp;
+    struct curl_slist* header_list = NULL;
+    CURL* curl_handle;
+    CURLcode res;
+    struct stat file_info;
+
+    stat(file, &file_info);
+    fp = fopen(file, "rb");
+    curl_handle = curl_easy_init();
+    header_list = curl_slist_append(header_list, "Content-Type: image/jpeg");
+
+    if (curl_handle)
+    {
+        curl_easy_setopt(curl_handle, CURLOPT_READFUNCTION, read_callback);
+        curl_easy_setopt(curl_handle, CURLOPT_UPLOAD, 1L);
+        curl_easy_setopt(curl_handle, CURLOPT_PUT, 1L);
+        curl_easy_setopt(curl_handle, CURLOPT_URL, server_target);
+        curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, header_list);
+        curl_easy_setopt(curl_handle, CURLOPT_READDATA, fp);
+        curl_easy_setopt(curl_handle, CURLOPT_INFILESIZE_LARGE, (curl_off_t)file_info.st_size);
+
+        res = curl_easy_perform(curl_handle);
+    }
+
+    fclose(fp);
+    curl_easy_cleanup(curl_handle);
+}
+
+static size_t read_callback(void* ptr, size_t size, size_t nmemb, void* stream)
+{
+    size_t retcode;
+    curl_off_t nread;
+
+    retcode = fread(ptr, size, nmemb, stream);
+
+    nread = (curl_off_t)retcode;
+    fprintf(stderr, "***We read %" CURL_FORMAT_CURL_OFF_T" bytes from file\n", nread);
+
+    return retcode;
+}
+
